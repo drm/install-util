04408d7084010a06cf5af9f2c01ce71531d3011b
Gerard van Helden <gerard@van-helden.name>
2023-12-10 23:44:14 +0100

    crud basics

---
+++ util/db.sh
@@ -0,0 +1,29 @@
+_query-select() {
+	local table="$1"
+	local where="${2:-1}"
+
+	_query <<< "SELECT * FROM $table WHERE $where"
+}
+
+
+_query-insert() {
+	local table="$1"
+	local columns=""
+	local values=""
+
+	for name in $(_query <<< "PRAGMA table_info($table);" | awk -F "|" '{print $2}'); do
+		if [ "$columns" != "" ]; then columns="$columns,"; fi;
+		if [ "$values" != "" ]; then values="$values,"; fi;
+		
+		read -p "$name: " val_$name;
+		value="val_${name}"
+		columns="$columns $name"
+		values="$values '${!value}'"
+	done
+
+	_query <<< "PRAGMA foreign_keys=ON; INSERT INTO $table($columns) VALUES($values);"
+}
+
+_query-add() {
+	_query-insert "$@"
+}
