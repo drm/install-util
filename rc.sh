eb1d4860d0836303137975fab8b507e555b890c5
Gerard van Helden <gerard@van-helden.name>
2024-03-29 18:48:10 +0100

    allow FORCE for bash < 5

--- rc.sh
+++ rc.sh
@@ -1,6 +1,8 @@
 if [ "${BASH_VERSINFO:-0}" -lt 5 ]; then
-  echo "Needs at least bash version 5"
-  exit 1
+	echo "Needs at least bash version 5"
+	if [ "${FORCE:-}" == "" ]; then
+		exit 1
+	fi
 fi
 
 ## Filters all var names from a `declare -p` output
