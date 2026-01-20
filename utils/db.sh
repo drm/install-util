d57346263e548ef84d2e090d09a22401f6a3181f
Gerard van Helden <gerard@van-helden.name>
2026-01-20 11:36:57 +0100

    default to NULL for empty values if column is nullable

--- utils/db.sh
+++ utils/db.sh
@@ -10,15 +10,21 @@ _query-insert() {
 	local table="$1"
 	local columns=""
 	local values=""
+	local notnull
 
 	for name in $(_query <<< "PRAGMA table_info($table);" | awk -F "|" '{print $2}'); do
+		notnull=$(_query <<< "SELECT \"notnull\" FROM pragma_table_info('$table') WHERE name='$name';")
 		if [ "$columns" != "" ]; then columns="$columns,"; fi;
 		if [ "$values" != "" ]; then values="$values,"; fi;
 
 		read -p "$name: " val_$name;
 		value="val_${name}"
 		columns="$columns $name"
-		values="$values '${!value}'"
+		if ! [ "${!value}" ] && [ "$notnull" -eq 0 ]; then
+			values="$values NULL"
+		else
+			values="$values '${!value}'"
+		fi
 	done
 
 	_query <<< "PRAGMA foreign_keys=ON; BEGIN; INSERT INTO $table($columns) VALUES($values); COMMIT;"
